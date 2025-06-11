1c68007ff2cded5a410ff0b81274b33d330abdbd
Leandro794 <44125429@fi.unju.edu.ar>
2025-06-11 02:51:12 -0300

    Creacion del componente favoritos para ver los productos marcados como favorito [Leandro Bejarano]

--- tp-integrador/src/views/Favoritos.jsx
+++ tp-integrador/src/views/Favoritos.jsx
@@ -1,5 +1,32 @@
-function Favoritos () {
-    return <h1>Favoritos</h1>
+import { useSelector, useDispatch } from "react-redux";
+import { toggleFavorito } from "../services/ProductsSlice";
+
+function Favoritos() {
+    const dispatch = useDispatch();
+    const productos = useSelector(state => state.products.lista)
+    const favoritos = useSelector(state => state.products.favoritos);
+
+    const productosFavoritos = productos.filter(p => favoritos.includes(p.id));
+    if (productosFavoritos.length === 0) return <p>No hay productos favoritos</p>
+    return (
+        <div>
+            <h2>Productos Favoritos</h2>
+            {productosFavoritos.map(producto => (
+                <div key={producto.id}>
+                    <h3>{producto.title}</h3>
+                    <img src={producto.image} alt={producto.title} width="100"/>
+                    <p>Precio: ${producto.price}</p>
+                    <p>Categoria: {producto.category}</p>
+                    <p>Descripcion: {producto.description}</p>
+                    <label>
+                        <input type="checkbox" checked={true} onChange={() => dispatch(toggleFavorito(producto.id))} />
+                        Quitar de favoritos
+                    </label>
+                    <hr />
+                </div>
+            ))}
+        </div>
+    )
 }
 
 export default Favoritos
