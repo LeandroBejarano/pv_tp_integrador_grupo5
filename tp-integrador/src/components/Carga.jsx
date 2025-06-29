d925f9784e18fb12c5ef6b644f941939a4e8d53b
Leandro794 <44125429@fi.unju.edu.ar>
2025-06-29 01:49:52 -0300

    Creo el componente reutilizable de Carga para mostrar un spinner de espera mientras se renderiza el contenido [Leandro Bejarano]

---
+++ tp-integrador/src/components/Carga.jsx
@@ -0,0 +1,10 @@
+function Carga() {
+    return (
+        <>
+            <p className="text-center my-5">Cargando página...</p>
+            <div class="spinner-border text-primary mx-auto d-block" role="status"></div>
+        </>
+    )
+}
+
+export default Carga
