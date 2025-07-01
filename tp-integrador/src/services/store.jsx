d856ea0643d078d0156ac2c48952eb6075a2b4d3
Leandro794 <44125429@fi.unju.edu.ar>
2025-07-01 17:19:28 -0300

    Agrego localStorage a ProductSlice.jsx y store.jsx •Ahora los cambios a los productos (crear, editar, eliminar) persistirán incluso si se actualiza la pagina [Leandro Bejarano]

--- tp-integrador/src/services/store.jsx
+++ tp-integrador/src/services/store.jsx
@@ -1,8 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
 import productsReducer from "./ProductsSlice"
 
+const PERSIST_KEY = 'productos_app_state'
+
+const persistedState = localStorage.getItem(PERSIST_KEY)
+    ? JSON.parse(localStorage.getItem(PERSIST_KEY))
+    : undefined;
+
 export const store = configureStore({
     reducer: {
         products: productsReducer,
-    }
+    },
+    preloadedState: persistedState,
 })
+
+store.subscribe(() => {
+    localStorage.setItem(PERSIST_KEY, JSON.stringify({
+        products: store.getState().products
+    }));
+});
